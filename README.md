81b8c697a22dd0a5f2b42c1611f4ba285e7bef32
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2025-01-21 08:52:56 +0100

    initialize project with Spring Initializr

---
+++ README.md
@@ -0,0 +1,1 @@
+### Spring Boot Exercises By Anthony Teye-Adjei
---
+++ src/test/java/com/tonyadjei/springbootbasics/SpringbootbasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyadjei.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
