cf817d20e4da23b5dd0af471e3fdd0a531da170d
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2025-01-21 09:26:56 +0100

    Exercise2: Calculator Controller

---
+++ src/main/java/com/tonyadjei/springbootbasics/Exercise2/CalculatorController.java
@@ -0,0 +1,21 @@
+package com.tonyadjei.springbootbasics.Exercise2;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("add")
+    public String addition(@RequestParam(required = false) Long a, @RequestParam(required = false) Long b) {
+        return a != null && b != null ? "The sum is: " + (a + b) : "Please add 2 query parameters, a and b in the url to perform an addition";
+    }
+
+    @GetMapping("multiply/{a}/{b}")
+    public String multiplication(@PathVariable(required = false) Long a, @PathVariable(required = false) Long b) {
+        return a != null && b != null ? "The product is: " + (a * b) : "Please add 2 path parameters, a and b in the url to perform a multiplication";
+    }
+}
